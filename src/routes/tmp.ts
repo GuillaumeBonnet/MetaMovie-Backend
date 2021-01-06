798e5beb928c1d0bf44940bf1468ecdd2210baba
Guillaume Bonnet <7252481+GuillaumeBonnet@users.noreply.github.com>
2021-01-06 22:51:40 +0100

    prettier conf

--- src/routes/tmp.ts
+++ src/routes/tmp.ts
@@ -1,7 +1,7 @@
 const tmp = {
-  titi: "toto",
-  get: (arg1: string) => {
-    console.log("gboDebug: original tmp get func");
-  },
+	titi: "toto",
+	get: (arg1: string) => {
+		console.log("gboDebug: original tmp get func");
+	},
 };
 export { tmp };
